9c54df8e5e4353dd2f5532e52d934466fdc9a0a5
Guilherme André Welter <guilherme.a.welter@gmail.com>
2019-10-18 12:44:15 -0300

    Criando lista de sufixos, prefixos e dominios

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,74 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="text-center">
+      <h1>Namegator</h1>
+      <br/>
+      <h6 class="text-secondary">Gerador de nomes com vue, node, boostrap, grahpQl</h6>
+    </div>
+    <div id="main">
+      <div class="container">
+        <div class="row">
+          <div class="col-md">
+            <h5>Prefixos <span class="badge badge-info">0</span></h5>
+            <div class="card">
+              <div class="card-body">
+                <ul class="list-group">
+                  <li class="list-group-item" v-for="prefix in prefixes" :key="prefix">{{prefix}}</li>
+                </ul>
+                <br/>
+                <input class="form-control" type="text" placeholder="Digite o prefixo" />
+              </div>
+            </div>
+          </div>
+          <div class="col-md">
+            <h5>Sufixos <span class="badge badge-info">0</span></h5>
+            <div class="card">
+              <div class="card-body">
+                <ul class="list-group">
+                  <li class="list-group-item" v-for="sufix in sufixes" :key="sufix">{{sufix}}</li>
+                </ul>
+                <br/>
+                <input class="form-control" type="text" placeholder="Digite o sufixo" />
+              </div>
+            </div>
+          </div>
+        </div>
+        <br/>
+        <h5>Domains <span class="badge badge-info">{{domains.length}}</span></h5>
+        <div class="card">
+          <div class="card-body">
+            <ul class="list-group">
+                <li class="list-group-item" v-for="domain in domains" :key="domain">{{domain}}</li>
+              </ul>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'font-awesome/css/font-awesome.css'
 
 export default {
   name: 'app',
-  components: {
-    HelloWorld
+  data () {
+    return {
+      prefixes: ['Air', 'Jet', 'Flight'],
+      sufixes: ['Hub', 'Station', 'Mart'],
+      domains: ['AirHub', 'AirStation', 'AirMart', 'JetHub', 'JetStation', 'JetMart', 'FlightHub', 'FlightStation', 'FlightMart'],
+    }
   }
 }
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  margin: 30px 0;
+}
+#main {
+  background-color: #f1f1f1;
+  padding: 30px 0;
 }
 </style>
