b38696551e79fe3c235085f196d1351e6964d2c4
Guilherme André Welter <guilherme.a.welter@gmail.com>
2019-10-18 13:38:24 -0300

    Configurando lint

--- src/App.vue
+++ src/App.vue
@@ -2,44 +2,53 @@
   <div id="app">
     <div class="text-center">
       <h1>Namegator</h1>
-      <br/>
+      <br />
       <h6 class="text-secondary">Gerador de nomes com vue, node, boostrap, grahpQl</h6>
     </div>
     <div id="main">
       <div class="container">
         <div class="row">
           <div class="col-md">
-            <h5>Prefixos <span class="badge badge-info">0</span></h5>
+            <h5>
+              Prefixos
+              <span class="badge badge-info">0</span>
+            </h5>
             <div class="card">
               <div class="card-body">
                 <ul class="list-group">
                   <li class="list-group-item" v-for="prefix in prefixes" :key="prefix">{{prefix}}</li>
                 </ul>
-                <br/>
+                <br />
                 <input class="form-control" type="text" placeholder="Digite o prefixo" />
               </div>
             </div>
           </div>
           <div class="col-md">
-            <h5>Sufixos <span class="badge badge-info">0</span></h5>
+            <h5>
+              Sufixos
+              <span class="badge badge-info">0</span>
+            </h5>
             <div class="card">
               <div class="card-body">
                 <ul class="list-group">
                   <li class="list-group-item" v-for="sufix in sufixes" :key="sufix">{{sufix}}</li>
                 </ul>
-                <br/>
+                <br />
                 <input class="form-control" type="text" placeholder="Digite o sufixo" />
               </div>
             </div>
           </div>
         </div>
-        <br/>
-        <h5>Domains <span class="badge badge-info">{{domains.length}}</span></h5>
+        <br />
+        <h5>
+          Domains
+          <span class="badge badge-info">{{domains.length}}</span>
+        </h5>
         <div class="card">
           <div class="card-body">
             <ul class="list-group">
-                <li class="list-group-item" v-for="domain in domains" :key="domain">{{domain}}</li>
-              </ul>
+              <li class="list-group-item" v-for="domain in domains" :key="domain">{{domain}}</li>
+            </ul>
           </div>
         </div>
       </div>
@@ -48,19 +57,29 @@
 </template>
 
 <script>
-import 'bootstrap/dist/css/bootstrap.css'
-import 'font-awesome/css/font-awesome.css'
+import "bootstrap/dist/css/bootstrap.css";
+import "font-awesome/css/font-awesome.css";
 
 export default {
-  name: 'app',
-  data () {
-    return {
-      prefixes: ['Air', 'Jet', 'Flight'],
-      sufixes: ['Hub', 'Station', 'Mart'],
-      domains: ['AirHub', 'AirStation', 'AirMart', 'JetHub', 'JetStation', 'JetMart', 'FlightHub', 'FlightStation', 'FlightMart'],
-    }
-  }
-}
+	name: "app",
+	data() {
+		return {
+			prefixes: ["Air", "Jet", "Flight"],
+			sufixes: ["Hub", "Station", "Mart"],
+			domains: [
+				"AirHub",
+				"AirStation",
+				"AirMart",
+				"JetHub",
+				"JetStation",
+				"JetMart",
+				"FlightHub",
+				"FlightStation",
+				"FlightMart"
+			]
+		};
+	}
+};
 </script>
 
 <style>
